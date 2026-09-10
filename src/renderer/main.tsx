b6810aec3648470543adb8c6b1b62f68dcf1e7c1
Aditya Yadav <93206009+Officially-aditya@users.noreply.github.com>
2026-09-10 09:18:25 +0530

    style: keep shared control skin last

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -6,8 +6,8 @@ import './navigation.css';
 import './settings.css';
 import './execution.css';
 import './react.css';
-import './controls.css';
 import './composer-refinements.css';
+import './controls.css';
 import { App } from './react/App';
 import './types';
 
