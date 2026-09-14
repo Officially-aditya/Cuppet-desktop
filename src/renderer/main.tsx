e7f7bf8b28ef891107700cd08afd4bc78e93d37d
Aditya Yadav <93206009+Officially-aditya@users.noreply.github.com>
2026-09-14 13:42:17 +0530

    style: normalize typography and icon geometry

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -26,6 +26,8 @@ import './conversation.css';
 import './composer.css';
 import './overlays.css';
 import './motion.css';
+import './typography.css';
+import './icons.css';
 import { App } from './react/App';
 import { WorkspaceEnhancements } from './react/WorkspaceEnhancements';
 import { ProjectTerminalMount } from './react/ProjectTerminalMount';
